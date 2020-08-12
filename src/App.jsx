1ed7732ff44c971d32c361ac05e5265e7c8fc589
Công Sơn <sonseait@gmail.com>
2020-08-13 00:54:27 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,26 @@ import * as React from "react";
 import { Globe } from "./dist";
 
 export default class App extends React.PureComponent {
-  state = {
-    leaved: false,
-  };
-
-  componentDidMount() {
-    // setTimeout(() => {
-    //   this.setState({
-    //     leaved: true,
-    //   });
-    // }, 5000);
-  }
+  componentDidMount() {}
 
   render() {
     return (
       <div className="blur_bottom" style={{ height: 440, overflow: "hidden" }}>
         <div style={{ height: 600 }}>
-          {!this.state.leaved && (
-            <Globe
-              lines={[
-                [38.2844, 210.532, 50.2844, 300.3383],
-                [38.2844, 175.532, 50.2844, 300.3383],
-                [35.2844, 250.3383, 38.2844, 319.3383],
-                [40.2844, 60.3383, 55.2844, 110.532],
-                [38.2844, 90.3383, 70.2844, 135.532],
-                [55.2844, 338.3383, 45.2844, 60.3383],
-              ]}
-              mapImageUrl="map1.png"
-              rotationAnimateAmount={{
-                y: -0.002,
-              }}
-            />
-          )}
+          <Globe
+            lines={[
+              [38.2844, 210.532, 50.2844, 300.3383],
+              [38.2844, 175.532, 50.2844, 300.3383],
+              [35.2844, 250.3383, 38.2844, 319.3383],
+              [40.2844, 60.3383, 55.2844, 110.532],
+              [38.2844, 90.3383, 70.2844, 135.532],
+              [55.2844, 338.3383, 45.2844, 60.3383],
+            ]}
+            mapImageUrl="map1.png"
+            rotationAnimateAmount={{
+              y: -0.002,
+            }}
+          />
         </div>
       </div>
     );
